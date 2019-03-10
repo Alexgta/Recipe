40fc041378a024333d5609c2708b630d1c4b3f49
Alexgta <9720560@gmail.com>
2019-03-10 13:45:51 +0300

    adding view for one ingredient

---
+++ src/main/java/ca/demo/services/IngredientService.java
@@ -0,0 +1,8 @@
+package ca.demo.services;
+
+import ca.demo.commands.IngredientCommand;
+
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}
